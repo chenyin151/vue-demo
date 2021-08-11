59089a86c2c06c19acb864d3e85929cb8af841af
STZBLZHNCJFDMIO\Administrator <chenyin151@163.com>
2021-08-12 00:33:39 +0800

    provide/inject

---
+++ src/components/provider/inject/child-one.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-12 00:27:11
+ * @LastEditTime: 2021-08-12 00:32:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-demo\src\components\provider\inject\child-one.vue
+-->
+<template>
+    <div>
+        {{demo}}
+        <child-two></child-two>
+    </div>
+</template>
+
+<script>
+    import ChildTwo from './child-two.vue'
+    export default {
+        name: 'childOne',
+        inject: ['message'],
+        data() {
+            return {
+                demo: this.message
+            }
+        },
+        components: {
+            ChildTwo
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/provider/inject/child-two.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-12 00:27:20
+ * @LastEditTime: 2021-08-12 00:33:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-demo\src\components\provider\inject\child-two.vue
+-->
+<template>
+    <div>
+        {{demo}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        inject: ['message'],
+        data() {
+            return {
+                demo: this.message
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
